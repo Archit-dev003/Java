38e0835d754b65a31ce264c126a37c3fa928a295
Archit Chhirolya <architchhirolya@gmail.com>
2026-09-10 17:57:49 +0530

    Concepts of primitive datatype and literals

---
+++ Core java/P02_Literal.java
@@ -0,0 +1,16 @@
+public class P02_Literal {
+    public static void main(String[] args) {
+        
+        int n = 10;
+        double a = 12e9; //varibale = literal
+        char c = 'a';
+        boolean value = true;
+        float f = 8.9f;
+
+        System.out.println("The value of n is: "+n);
+        System.out.println("The exponential value of a is: "+a);
+        System.out.println("The character held in c is: "+c);
+        System.out.println("Is java object oriented?: "+value);
+        System.out.println("Value of f is: "+f);
+    }
+}

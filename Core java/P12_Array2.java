7dc0b9b6fe268ffae4ba9f06dc3e76f81a7a3829
Archit Chhirolya <architchhirolya@gmail.com>
2026-09-16 01:28:55 +0530

    Use of 3D array and jagged array in java

---
+++ Core java/P12_Array2.java
@@ -0,0 +1,28 @@
+public class P12_Array2 {
+    public static void main(String[] args) {
+        
+        int arr[][] = new int[3][];
+
+        arr[0] = new int[3];
+        arr[1] = new int[4];
+        arr[2] = new int[5];
+
+        // for(int i=0;i<arr.length;i++)
+        // {
+        //     for(int j=0;j<arr[i].length;j++)
+        //     {
+        //         System.out.print(arr[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+        for(int[] n: arr)
+        {
+            for(int v:n)
+            {
+                System.out.print(v+" ");
+            }
+            System.out.println();
+        }
+    }
+}

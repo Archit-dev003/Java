0a91c1e60e43282e977b9d8e1ad53a659a3e9c9f
Archit Chhirolya <architchhirolya@gmail.com>
2026-09-24 23:19:25 +0530

    Implementing concept of polymorphism in java

---
+++ Practice Qs/P31_Polymorphism.java
@@ -0,0 +1,50 @@
+class Vehicle
+{
+    private String Brand;
+    public void start()
+    {
+        System.out.println("Vehicle Start");
+    }
+    
+    public String getBrand()
+    {
+        return Brand;
+    }
+}
+
+class Car extends Vehicle
+{
+    int seat;
+    @Override 
+    public void start()
+    {
+        System.out.println("Car Start");
+    }
+}
+
+class Bike extends Vehicle
+{
+    Boolean HaveGear;
+    @Override 
+    public void start()
+    {
+        System.out.println("Bike Start");
+    }
+}
+
+public class P31_Polymorphism {
+    public static void main(String[] args) {
+        
+        Vehicle V[] = new Vehicle[4];
+        V[0] = new Bike();
+        V[1] = new Car();
+        V[2] = new Bike();
+        V[3] = new Car();
+
+        for(int i = 0; i<V.length; i++)
+        {
+            V[i].start();
+        }
+
+    }    
+}
